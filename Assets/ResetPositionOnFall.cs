0f4a22fc88a28018e8e9ecfb53523da2f78fd6bc
Observer46 <microplayerpl@gmail.com>
2022-06-20 02:15:48 +0200

    final final release

--- Assets/ResetPositionOnFall.cs
+++ Assets/ResetPositionOnFall.cs
@@ -4,26 +4,11 @@ using UnityEngine;
 
 public class ResetPositionOnFall : MonoBehaviour
 {
+    public GameObject resetPoint;
 
-    public Vector3 initialPosition;
-    public bool useCurrentPosition;
-    public float heightLimit = -15f;
-    // Start is called before the first frame update
-    void Start()
-    {
-       if (useCurrentPosition)
-       {
-           initialPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-       }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(transform.position.y <= heightLimit)
-        {
-            Debug.Log("Resetting " + this +" position...");
-            transform.position = initialPosition;
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            other.transform.position = resetPoint.transform.position;
         }
     }
 }
